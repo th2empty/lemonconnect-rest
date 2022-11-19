033448b3a1dd061dec59011eb188b5339c83f656
th2empty <th2empty@gmail.com>
2022-11-19 17:12:43 +0500

    init

---
+++ src/main/kotlin/ru/lemon4k/lemonconnect/LemonConnectApplication.kt
@@ -0,0 +1,11 @@
+package ru.lemon4k.lemonconnect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LemonConnectApplication
+
+fun main(args: Array<String>) {
+    runApplication<LemonConnectApplication>(*args)
+}
---
+++ src/test/kotlin/ru/lemon4k/lemonconnect/LemonConnectApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.lemon4k.lemonconnect
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LemonConnectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
